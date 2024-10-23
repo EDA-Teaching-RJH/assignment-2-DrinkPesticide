d060d298da5754df72745f0c453fac9d19547c0c
DrinkPesticide <adamlkrogers@yahoo.com>
2024-10-23 16:58:28 +0100

    Tried to fix pt. 3

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,10 +1,18 @@
 def main():
     numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+    print(numbers)
     numbers.sort()
     # sorts the list in ascending order
     print(numbers)
+    for i in range(len(numbers)):
+        if i == 1:
+            del numbers[i]
+        elif i != 1:
+            continue
+    print(numbers)
     # remove number if it is 1
     # Adds numbers 7 and 8 at the end of the list.
-    numbers.extend()
-    # prints the finals list
+
+    # prints the final list
+    print(numbers)
 main()
